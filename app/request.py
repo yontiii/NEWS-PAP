5ee5643aaa24166c689f258aa01c63c866ed71f3
yontey <muasajohn01@gmail.com>
2019-04-14 23:19:37 +0300

    created function to display sources information

--- app/request.py
+++ app/request.py
@@ -1,4 +1,24 @@
 from app import app
+import urllib.request,json
+from .models import articles,news,headlines
+
+Articles = articles.Articles
+News = news.News
+Headlines = headlines.Headlines
+
 
 api_key = app.congi]['NEWS_API_KEY']
+
+# Getting the base urls
+
+sources_url = app.config['SOURCES_URL']
+articles_url = app.config['Articles_URL']
+headlines_url = app.config['  HEADLINES_URL']
+
+def get_sources(category):
+     '''
+    Function that gets the json response to our url request
+    '''
+    
+    
 
