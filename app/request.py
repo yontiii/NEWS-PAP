e0fb599499872683c80e436e59f87e1a55ffe222
yontey <muasajohn01@gmail.com>
2019-04-14 23:28:37 +0300

    added process sources function to conver the sources results into a dictionary of objects

--- app/request.py
+++ app/request.py
@@ -20,5 +20,27 @@ def get_sources(category):
     Function that gets the json response to our url request
     '''
     
+    get_sources_url = sources_url.format(category,api_key)
+    
+    with urllib.request.urlopen(get_sources_url) as url:
+        sources_data = url.read()
+        sources_response = json.loads(sources_data)
+        
+        sources_results = None
+        
+        if sources_response['sources']:
+            sources_list = sources_response['sources']
+            
+            sources_results = process_sources(source_results_list)
+            
+        return sources_results
+    
+def process_sources(sources_list):
+    '''
+    Function that processesses the source results and turns them to a list of objects
+    '''
+    
+            
+    
     
 
