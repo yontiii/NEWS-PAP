52f986626ad91f7332086ac90916c448bf15b1fe
yontey <muasajohn01@gmail.com>
2019-04-14 23:02:03 +0300

    added a headlines class

--- app/models/articles.py
+++ app/models/articles.py
@@ -9,3 +9,8 @@ class Articles:
     self.name = name 
     self.author = author
     self.title = title
+    self.description = description
+    self.url = url 
+    self.urlToImage = urlToImage
+    self.publishedAt = publishedAt
+    self.content = content 
---
+++ app/models/headlines.py
@@ -0,0 +1,6 @@
+class Headlines:
+    '''
+    Class to define the instances of new headlines
+    '''
+    
+    def __init__(self,)
